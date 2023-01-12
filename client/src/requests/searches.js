498a4562c9da15cafbf441bbd13672895b29730e
David Vu <davidvu7887@gmail.com>
2023-01-11 23:36:42 -0800

    API debugging

--- client/src/requests/searches.js
+++ client/src/requests/searches.js
@@ -4,8 +4,10 @@ export const getImages = async (term, start, imagesToSearch, fileTypeOption) => 
     let requestURL = `${baseURL}?q=${term}&start=${start}&num=${imagesToSearch}&fileType=${fileTypeOption}`;
     try {
         let response = await fetch(requestURL);
+        console.log(response);
         if (response.ok) {
             let responseJSON = await response.json();
+            console.log(responseJSON);
             return responseJSON;
         }
     } catch (error) {
