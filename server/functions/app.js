8b239096fe01ddbd8996112f6da71f622e9251ab
David Vu <davidvu7887@gmail.com>
2023-01-11 23:08:05 -0800

    Testing

--- server/functions/app.js
+++ server/functions/app.js
@@ -24,14 +24,13 @@ app.use(bodyParser.json({limit:'1kb'}));
 app.use(bodyParser.urlencoded({extended: true, limit:'1kb'}));
 app.use(xss());
 
-app.get("/.netlify/functions/app/api/images", async (req, res, next) => {
-    // const term = req.query.q;
-    // const start = req.query.start;
-    // const imagesToSearch = req.query.num;
-    // const fileTypeOption = req.query.fileType;
-    // let allImages = await fetchImages(term, start, imagesToSearch, fileTypeOption);
-    // res.status(200).json(allImages);
-    res.status(200).json({"COOL": "YOU MADE IT"});
+app.get("/.netlify/functions/app/api/images/", async (req, res, next) => {
+    const term = req.query.q;
+    const start = req.query.start;
+    const imagesToSearch = req.query.num;
+    const fileTypeOption = req.query.fileType;
+    let allImages = await fetchImages(term, start, imagesToSearch, fileTypeOption);
+    res.status(200).json(allImages);
 })
 
 app.get("/.netlify/functions/app/", (req, res) => {
